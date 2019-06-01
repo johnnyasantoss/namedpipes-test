9b8dee1a8bf640b4d3c4b650460594df905ed798
Johnny Anderson dos Santos <johnny.santos@nmbrs.nl>
2019-06-01 16:47:00 +0200

    named pipes test

---
+++ client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SendInfoToLocalPipe();
+        }
+
+        private static async Task SendInfoToLocalPipe()
+        {
+            using (var pipe = new NamedPipeClientStream(".", "test", PipeDirection.InOut))
+            {
+                Console.WriteLine("Connecting...");
+                await pipe.ConnectAsync(3000);
+                Console.WriteLine("Conected.");
+
+                int i = 0;
+                using (var sw = new StreamWriter(pipe))
+                {
+                    sw.AutoFlush = true;
+                    while (Console.ReadKey(false).Key != ConsoleKey.Escape)
+                    {
+                        await sw.WriteLineAsync("Test " + ++i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var pipe = new NamedPipeServerStream("test", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous))
+            {
+                Console.WriteLine("Waiting... ");
+                await pipe.WaitForConnectionAsync();
+                Console.WriteLine("Connected? " + pipe.IsConnected);
+
+                var shouldStop = false;
+                Console.CancelKeyPress += delegate
+                {
+                    Console.WriteLine("Stopping...");
+                    shouldStop = true;
+                };
+
+                using (var sr = new StreamReader(pipe))
+                {
+                    while (!shouldStop)
+                    {
+                        Console.WriteLine(await sr.ReadLineAsync());
+                    }
+                }
+
+                if (pipe.IsConnected)
+                {
+                    Console.WriteLine("Draining...");
+                    pipe.WaitForPipeDrain();
+                }
+            }
+        }
+    }
+}
